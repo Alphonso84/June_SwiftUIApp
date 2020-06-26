ee08ad2eb9b9ddd9166853326572863e4acffc31
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2020-06-26 08:55:20 -0700

    Initial Commit

---
+++ June_SwiftUIApp/June_SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  June_SwiftUIAppApp.swift
+//  June_SwiftUIApp
+//
+//  Created by Alphonso Sensley II on 6/26/20.
+//
+
+import SwiftUI
+
+@main
+struct June_SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
